7d060537e9bae71834840173298cfea0f04f49e8
christopher <christopher.kade@epitech.eu>
2017-05-02 16:25:07 +0200

    fixed heroku

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+// const https = require('https');
 const bodyParser = require('body-parser');
 
 // Get our API routes
@@ -16,10 +17,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(__dirname + '/dist'));
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  // res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3000');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -35,28 +36,42 @@ app.use(function (req, res, next) {
   next();
 });
 
+// const forceSSL = function() {
+//   return function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       return res.redirect(
+//         ['https://', req.get('Host'), req.url].join('')
+//       );
+//     }
+//     next();
+//   }
+// };
+//
+// app.use(forceSSL());
+
 // Set our api routes
 app.use('/api', api);
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/src/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
 /**
  * Get port from environment and store in Express.
  */
-const port = process.env.PORT|| '3000';
+const port = process.env.PORT || '3000';
 app.set('port', port);
 
 /**
  * Create HTTP server.
  */
 const server = http.createServer(app);
+// const server = https.createServer(app);
 
 /**
  * Listen on provided port, on all network interfaces.
  */
-server.listen(port, "0.0.0.0");
+// server.listen(port, "0.0.0.0");
 server.listen(port, () => console.log(`API running on localhost:${port}`));
 
