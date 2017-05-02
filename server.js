3bb3986d3644e3e18dbd969b84bd09ef052390fe
christopher <christopher.kade@epitech.eu>
2017-05-02 16:59:54 +0200

    fixed heroku

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/dist'));
 
 app.use(function (req, res, next) {
-  // res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
